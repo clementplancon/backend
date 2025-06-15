7564af0d98284dc62984aaf4f75872e9c0489a9a
clementplancon <clement.plancon.pro@gmail.com>
2025-06-15 15:34:50 +0200

    feat(websocket): add WebSocket adapter for enhanced real-time communication

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
     ],
     credentials: true,
   });
+
+  // **Ajoute ceci**
+  app.useWebSocketAdapter(new IoAdapter(app));
 
   await app.listen(3000, '0.0.0.0');
 }
